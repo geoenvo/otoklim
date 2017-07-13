ac197dbee105343c2b0d41cbc2867d142fe5eea2
faizalprbw <faizalprbw@gmail.com>
2017-07-13 16:27:25 +0700

    add :station_id.csv & input logic

--- otoklim.py
+++ otoklim.py
@@ -28,7 +28,10 @@ import os
 import time
 import processing
 import math
+import datetime
 import shutil
+import logging
+import numpy as np
 
 from otoklim_dialog import OtoklimDialog
 from osgeo import gdal, ogr, osr
@@ -287,44 +290,10 @@ class Otoklim:
         bathymetry_file = os.path.join(plugin_dir, 'static\byth_gebco.tif')
         rule_file_ch = os.path.join(plugin_dir, 'static\rule_ch.txt')
         rule_file_sh = os.path.join(plugin_dir, 'static\rule_sh.txt')
+        driver = ogr.GetDriverByName("ESRI Shapefile")
         # See if OK was pressed
         if result:
             # GEO 03062017 Insert Otoklim Main Logic
-            '''
-            filename_1 = self.dlg.Input_CH_CSV.text()
-            filename_2 = self.dlg.Delimiter.text()
-            filename_3 = self.dlg.Year.text()
-            filename_4 = self.dlg.Select_Months.currentText()
-            print filename_1
-            print filename_2
-            print filename_3
-            print filename_4
-            '''
-            file_input = self.dlg.Input_CH_CSV.text()
-            csv_delimiter = self.dlg.Delimiter.text()
-            file_directory = str(os.path.dirname(os.path.realpath(file_input)))
-
-            map_directory = os.path.join(file_directory, 'map')
-            try:
-                shutil.rmtree(map_directory)
-                os.mkdir(os.path.join(file_directory, 'map'))
-            except:
-                os.mkdir(os.path.join(file_directory, 'map'))
-
-            temp_directory = os.path.join(file_directory, 'temp')
-            try:
-                shutil.rmtree(temp_directory)
-                os.mkdir(os.path.join(file_directory, 'temp'))
-            except:
-                os.mkdir(os.path.join(file_directory, 'temp'))
-
-            csv_directory = os.path.join(file_directory, 'csv')
-            try:
-                shutil.rmtree(csv_directory)
-                os.mkdir(os.path.join(file_directory, 'csv'))
-            except:
-                os.mkdir(os.path.join(file_directory, 'csv'))
-
             def select_date_now():
                 mth = self.dlg.Select_Months.currentText()
                 if mth == 'Januari':
@@ -351,7 +320,7 @@ class Otoklim:
                     mth = 11
                 else:
                     mth = 12
- 
+
                 month_dict = {
                     0: ['DES', 'DESEMBER'],
                     1: ['JAN', 'JANUARI'],
@@ -377,7 +346,15 @@ class Otoklim:
                 pmth_3 = month_dict[mth+3]
                 month_header = [amth, pmth_1, pmth_2, pmth_3]
 
-                yrs = int(float(self.dlg.Year.text()))
+                try:
+                    yrs = self.dlg.Year.text()
+                    if yrs:
+                        yrs = int(float(yrs))
+                    else:
+                        yrs = datetime.datetime.now().year
+                except:
+                    logger.warn('Error: Make sure input year is numeric') # QGIS Dialog
+                    pass
                 ayrs = yrs
                 pyrs_1 = pyrs_2 = pyrs_3 = yrs
                 if mth == 12:
@@ -392,14 +369,144 @@ class Otoklim:
                 elif mth == 1:
                     ayrs = yrs - 1
                 years_header = [ayrs, pyrs_1, pyrs_2, pyrs_3]
-
+                logger.info('month: ' + str(month_header))
+                logger.info('years: ' + str(years_header))
                 return month_header, years_header
-            print select_date_now()
-            print file_input
-            print csv_delimiter
-            items = []
-            for index in xrange(self.dlg.listWidget_selected.count()):
-                items.append(self.dlg.listWidget_selected.item(index))
-            selected_adm = [i.whatsThis() for i in items]
-            print selected_adm
-            pass
+
+            def select_adm_region():
+                if self.dlg.mode_1.isChecked():
+                    mode = 1
+                elif self.dlg.mode_2.isChecked():
+                    mode = 2
+                elif self.dlg.mode_3.isChecked():
+                    mode = 3
+                else:
+                    mode = 4
+                regional_list = []
+                regional_list.append(['provinsi', 'JAWA TIMUR', 35, 'none'])
+                if mode == 1:
+                    logger.info('Running mode : ' + str(mode) + ' [Jawa Timut Province Only]')
+                    logger.info(str(regional_list))
+                    return regional_list
+                elif mode == 2:
+                    logger.info('Running mode : ' + str(mode) + ' [All Kabupaten \ Kota]')
+                    dataSource = driver.Open(kabupaten_kota_polygon_file, 0)
+                    layer_kab = dataSource.GetLayer()
+                    for feature in layer_kab:
+                        regional_list.append(['kabupaten_kota', feature.GetField("KABUPATEN"), feature.GetField("ID_KAB_CLE"),  feature.GetField("ID_PROV")])
+                    logger.info(str(regional_list))
+                    return regional_list
+                elif mode == 3:
+                    logger.info('Running mode : ' + str(mode) + ' [All Kecamatan]')
+                    dataSource = driver.Open(kecamatan_polygon_file, 0)
+                    layer_kec = dataSource.GetLayer()
+                    for feature in layer_kec:
+                        regional_list.append(['kecamatan', feature.GetField("KECAMATAN"), feature.GetField("ID_KEC_CLE"),  feature.GetField("ID_KAB_CLE")])
+                    logger.info(str(regional_list))
+                    return regional_list
+                elif mode == 4:
+                    logger.info('Running mode : ' + str(mode) + ' [Selected Region]')
+                    items = []
+                    for index in xrange(self.dlg.listWidget_selected.count()):
+                        items.append(self.dlg.listWidget_selected.item(index))
+                    slc = [int(float(i.whatsThis())) for i in items]
+                    dataSource = driver.Open(kabupaten_kota_polygon_file, 0)
+                    layer_kab = dataSource.GetLayer()
+                    for feature in layer_kab:
+                        if int(feature.GetField("ID_KAB_CLE")) in slc:
+                            regional_list.append(['kabupaten_kota', feature.GetField("KABUPATEN"), feature.GetField("ID_KAB_CLE"),  feature.GetField("ID_PROV")])
+                    dataSource = driver.Open(kecamatan_polygon_file, 0)
+                    layer_kec = dataSource.GetLayer()
+                    for feature in layer_kec:
+                        if int(feature.GetField("ID_KEC_CLE")) in slc:
+                            regional_list.append(['kecamatan', feature.GetField("KECAMATAN"), feature.GetField("ID_KEC_CLE"),  feature.GetField("ID_KAB_CLE")])
+                    logger.info(str(regional_list))
+                    return regional_list
+
+            try:
+                after_prov = False
+                start_time = time.time()
+                now = datetime.datetime.now()
+
+                file_input = self.dlg.Input_CH_CSV.text()
+                csv_delimiter = self.dlg.Delimiter.text()
+                file_directory = str(os.path.dirname(os.path.realpath(file_input)))
+
+                map_directory = os.path.join(file_directory, 'map')
+                try:
+                    shutil.rmtree(map_directory)
+                    os.mkdir(os.path.join(file_directory, 'map'))
+                except:
+                    os.mkdir(os.path.join(file_directory, 'map'))
+
+                temp_directory = os.path.join(file_directory, 'temp')
+                try:
+                    shutil.rmtree(temp_directory)
+                    os.mkdir(os.path.join(file_directory, 'temp'))
+                except:
+                    os.mkdir(os.path.join(file_directory, 'temp'))
+
+                csv_directory = os.path.join(file_directory, 'csv')
+                try:
+                    shutil.rmtree(csv_directory)
+                    os.mkdir(os.path.join(file_directory, 'csv'))
+                except:
+                    os.mkdir(os.path.join(file_directory, 'csv'))
+
+                log_filename = os.path.join(file_directory, 'otoklim_' + '{:%Y%m%d_%H%M%S}'.format(datetime.datetime.now()) + '.log')
+
+                try:
+                    os.remove(log_filename)
+                except OSError:
+                    pass
+                logger = logging.getLogger(__name__)
+                logger.setLevel(logging.DEBUG)
+                ch = logging.StreamHandler()
+                ch.setLevel(logging.DEBUG)
+                fh = logging.FileHandler(log_filename)
+                formatter = logging.Formatter("%(asctime)s - [%(levelname)s] %(message)s")
+                ch.setFormatter(formatter)
+                fh.setFormatter(formatter)
+                logger.addHandler(ch)
+                logger.addHandler(fh)
+                logger.info('Running start at ' + '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+                slcs_date = select_date_now()
+                slcs_adm = select_adm_region()
+                print slcs_date
+                print slcs_adm
+            except:
+                pass
+                '''
+                csv_input = combinecsv(csv_delimiter)
+                point_stn = csvtoshp(csv_delimiter, csv_input)
+                raster_idw = idw_interpolate(point_stn, slcs_date[0])
+                idw_param = raster_idw[1]
+                for slc_adm in slcs_adm:
+                    logger.info('Region In Progress : ' + str(slc_adm))
+                    raster_clip = clip_raster(raster_idw, slc_adm)
+                    qgs_files = create_qgs(raster_clip, point_stn, slc_adm)
+                    create_pdf_map(qgs_files, slc_adm, slcs_date)
+                    if not after_prov:
+                        raster_cls = raster_classify(raster_clip)
+                        create_csv(idw_param, raster_cls)
+                    if after_prov:
+                        for raster in raster_clip[0]:
+                            os.remove(raster)
+                    after_prov = True
+
+                # Clear unuse file
+                for raster in raster_idw[0]:
+                    os.remove(raster)
+                for file in os.listdir(temp_directory):
+                    if file.split('.')[-1] != 'qgs':
+                        try:
+                            os.remove(os.path.join(temp_directory, file))
+                        except:
+                            pass
+
+                now = datetime.datetime.now()
+                logger.info('Running stop at ' + '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+                logger.info("--- %s seconds ---" % (time.time() - start_time))
+            except Exception as e:
+                logger.error(e)
+            '''
